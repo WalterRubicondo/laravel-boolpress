36ad470903b8ac94d756a37c21ebe949f5679816
Walter <walterrubicondo@gmail.com>
2021-05-26 17:57:33 +0200

    Fine Esercizio. Sass da fare

---
+++ resources/views/guests/index.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ route('admin.index') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    BOOLPRESS
+                </div>
+
+                <div class="row justify-content-center">
+      @foreach ($posts as $post)
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header"><a href="{{route('posts.show', ['slug' => $post->slug])}}">{{$post->title}}</a></div>
+
+                <div class="card-body">
+                  {{$post->content}}
+
+                  <div class="">
+                    <a href="{{route('admin.posts.edit', ['post' => $post->id])}}">Edit</a>
+                    <form action="{{route('admin.posts.destroy', ['post' => $post->id])}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" name="" value="Delete">
+                    </form>
+                  </div>
+                </div>
+            </div>
+        </div>
+      @endforeach
+    </div>
+            </div>
+        </div>
+    </body>
+</html>
